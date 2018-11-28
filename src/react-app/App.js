f096adb286f28361545e7433b8d8c4dc9c817fd7
Serge Uniquite <sergeuniquite@gmail.com>
2018-11-28 13:24:13 +0300

    init

--- src/react-app/App.js
+++ src/react-app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 
 /* redux actions */
 import ACTIONS, {  } from './redux/actions'
@@ -30,21 +31,53 @@ class App extends Component {
 	
 	render() {
 		const
-			{  } = this.props; /* redux */
+			{ INPUT_VALUE } = this.props; /* redux */
 			
-		return <div id="app" className="app">
-			<p style={{position: 'absolute', top: 0, left: 0}}>
-				App
-			</p>
-		</div>
+		return <Router>
+			<div id="app" className="app">
+				<p style={{position: 'absolute', top: 0, left: 0}}>
+					App
+				</p>
+
+				{ INPUT_VALUE }
+
+				<ul>
+					<li>
+						<Link to={'/aparts'}>
+							aparts
+						</Link>
+					</li>
+					<li>
+						<Link to={'/aparts/params'}>
+							params
+						</Link>
+					</li>
+				</ul>
+				
+				<Route exact path={'/aparts'} component={Home}>
+				</Route>
+
+				<Route path={'/aparts/params'} component={Params}>
+				</Route>
+			</div>
+		</Router>
 	}
 }
+
+const Home = () =>
+    <div>
+      <h2>Home</h2>
+    </div>,
+	Params = () => <div>
+      <h2>Params</h2>
+    </div>;
 
 /* ... . .-. --. . / --.. .... ..- .-. .- ...- .-.. . ...- */
 
 const
-	mapStateToProps = ({  }) => {
+	mapStateToProps = ({ controlled: { INPUT_VALUE } }) => {
 		return {
+			INPUT_VALUE
 		};
 	},
 	mapDispatchToProps = dispatch => ({
