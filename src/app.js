e40ee4bca33095914070e05716ad01aee27b87dd
Serge Uniquite <sergeuniquite@gmail.com>
2018-11-28 16:47:11 +0300

    parse hash && link href replaced

--- src/app.js
+++ src/app.js
@@ -30,6 +30,24 @@ document.querySelectorAll('#test2')[0].addEventListener('click', (e) => {
     console.log('test2');
 }, false);
 
+document.querySelectorAll('#test3')[0].addEventListener('click', (e) => {
+    console.log('test3');
+    e.preventDefault();
+    history.pushState(null, '', 'test3');
+}, false);
+
+document.querySelectorAll('#test4')[0].addEventListener('click', (e) => {
+    console.log('test4');
+    e.preventDefault();
+    history.pushState(null, '', 'test4');
+}, false);
+
+document.querySelectorAll('#test5')[0].addEventListener('click', (e) => {
+    console.log('test5');
+    e.preventDefault();
+    history.pushState(null, '', 'test5');
+}, false);
+
 const stateObj = { foo: "bar" };
 
 /* setTimeout(() => {
@@ -40,3 +58,25 @@ setTimeout(() => {
     console.log('window.history', window.history);
 }, 2000);
  */
+
+const parseHash = hash => {
+    const
+        setup = {},
+        sub = hash[0] === '#' ? hash.substr(1) : hash,
+        pairs = sub.split('#') || [];
+
+    pairs.forEach(pair => {
+        const
+            arr = pair.split('='),
+            key = arr[0],
+            val = arr[1];
+
+        setup[key] = val;
+    });
+
+    return setup;
+};
+
+const setup = parseHash(window.location.hash);
+
+console.log('setup', setup);
